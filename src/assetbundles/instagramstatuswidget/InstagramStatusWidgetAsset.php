164fe89a7f1b087d8fe97699867ad67909ec4918
John Morton <john@johnfmorton.com>
2017-12-07 18:09:09 -0500

    icons for widgets

---
+++ src/assetbundles/instagramstatuswidget/InstagramStatusWidgetAsset.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Pic Puller plugin for Craft CMS 3.x
+ *
+ * Integrate Instagram into Craft CMS.
+ *
+ * @link      https://picpuller.com
+ * @copyright Copyright (c) 2017 John F Morton
+ */
+
+namespace jmx2\picpuller\assetbundles\instagramstatuswidget;
+
+use Craft;
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
+
+/**
+ * LatestImageWidgetAsset AssetBundle
+ *
+ * AssetBundle represents a collection of asset files, such as CSS, JS, images.
+ *
+ * Each asset bundle has a unique name that globally identifies it among all asset bundles used in an application.
+ * The name is the [fully qualified class name](http://php.net/manual/en/language.namespaces.rules.php)
+ * of the class representing it.
+ *
+ * An asset bundle can depend on other asset bundles. When registering an asset bundle
+ * with a view, all its dependent asset bundles will be automatically registered.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-structure-assets.html
+ *
+ * @author    John F Morton
+ * @package   PicPuller
+ * @since     3.0.0
+ */
+class InstagramStatusWidgetAsset extends AssetBundle
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Initializes the bundle.
+     */
+    public function init()
+    {
+        // define the path that your publishable resources live
+        $this->sourcePath = "@jmx2/picpuller/assetbundles/instagramstatuswidget/dist";
+
+        // define the dependencies
+        $this->depends = [
+            CpAsset::class,
+        ];
+
+        // define the relative path to CSS/JS files that should be registered with the page
+        // when this asset bundle is registered
+        $this->js = [
+            'js/InstagramStatus.js',
+        ];
+
+        $this->css = [
+            'css/InstagramStatus.css',
+        ];
+
+        parent::init();
+    }
+}
